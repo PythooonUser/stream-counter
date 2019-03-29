b727f11ffae7b0dc00bd518bea1b48b8c2ac4dd8
Felix Siebeneicker <felix.sieben@t-online.de>
2019-03-29 23:25:52 +0100

    Adding counter

---
+++ stream-counter.js
@@ -0,0 +1,16 @@
+let countdown = 10;
+let label = document.querySelector(".js-stream-counter");
+
+let streamCounter = setInterval(function() {
+    if (countdown <= 0) {
+        clearInterval(streamCounter);
+        label.innerHTML = "NOW";
+        label.classList.add("animated", "infinite", "pulse", "slow");
+        return;
+    }
+
+    let minutes = Math.floor(countdown / 60);
+    let seconds = countdown - minutes * 60;
+    label.innerHTML = ((String(minutes).length <= 1) ? "0" : "") + minutes + ":" + ((String(seconds).length <= 1) ? "0" : "") + seconds;
+    countdown -= 1;
+}, 1000);
